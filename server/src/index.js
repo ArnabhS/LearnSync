34a7a47e54ae860b7d4a65b7232edb2f9503c351
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-10-20 14:03:10 +0530

    deploy fix

--- server/src/index.js
+++ server/src/index.js
@@ -14,9 +14,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
